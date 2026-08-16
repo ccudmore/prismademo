ca4f9cee5c8360ab97003086a6d8cfb009607913
ccudmore <craig@cudmore.ca>
2026-08-15 20:51:26 -0400

    updates

--- src/routes/Demo/+page.svelte
+++ src/routes/Demo/+page.svelte
@@ -4,10 +4,11 @@
 
 <h1>SvelteKit + Prisma</h1>
 
-{#each data.users as user} // [!code ++]
+{#each data.users as user}
 <h2>{user.name}</h2>
-// [!code ++] {#each user.posts as post}
+ {#each user.posts as post}
 <ul>
   <li><a href="{post.content}">{post.title}</a></li>
 </ul>
-// [!code ++] {/each} // [!code ++] {/each}
+ {/each}
+ {/each}
